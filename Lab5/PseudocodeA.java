33f7401aa477f9a6b27fa259d07efa9fb60bfe96
Algorithim <38524413+Algorithim@users.noreply.github.com>
2019-05-06 19:14:35 +0800

    Labs3-6

---
+++ Lab5/PseudocodeA.java
@@ -0,0 +1,24 @@
+/**
+ * Write a description of class Lab3 here.
+ * 
+ * @ SULEIMAN HASSAN U14/FNS/CSC/020
+ */
+import java.util.Scanner;
+public class Pseudocode
+{
+ public static void main(String[]arg){
+     int grade;
+     double average;
+     int total=0;
+     int counter=1;
+    Scanner input = new Scanner(System.in);
+    while(counter<=10){
+    System.out.println("Enter enter next grade");
+    grade=input.nextInt();
+    total=total+grade;
+    counter=counter+1;
+}
+    average = total/10;
+    System.out.println("The class avarage: "+average);
+}
+}
